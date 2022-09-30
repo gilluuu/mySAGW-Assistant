c5904824225bda8ddc672b18f124814f4e631a6f
Gilles Nikles <gilles.nikles@gmail.com>
2022-09-30 10:32:50 +0200

    Added exits, added warning, prevent failures of program

--- mySAGW-Assistant.py
+++ mySAGW-Assistant.py
@@ -10,7 +10,7 @@ from tkinter import filedialog
 w = 0
 x = 0
 y = 0
-z = 0
+z = "*"
 n_hm = True
 
 # Willkommensmeldung
@@ -19,89 +19,127 @@ print("Guten Tag,")
 print("Willkommen beim digitalen mySAGW-Assistenten!")
 print("Bitte wählen Sie die Dateien aus (Mehrfachauswahl möglich)")
 print()
+print(25*z)
+print("Achtung: Bitte sicherstellen, dass die nachfolgenden Spaltenüberschriften in Grossbuchstaben vorliegen:")
+print()
+print("SEKTION")
+print("MITGLIEDINSTITUTION")
+print("FORM_ID")
+print("REFERENZ-NR.")
+print(25*z)
 
 # Hauptprogramm
 # 1 Separate Listen, aufgeteilt nach Sektionen
 root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilenames(title="Dateien öffnen (Mehrfachauswahl möglich)")
-print(file_path)
 a = len(file_path)
-print(a)
+if file_path == "":
+    print("Keine Datei ausgewählt, Programm wird beendet")
+    sys.exit(0)
 
 print()
 print("Wo soll der Output gespeichert werden?")
 path = askdirectory(title='Speicherort auswählen')
+if path == "":
+    print("Kein Speicherort ausgewählt, Programm wird beendet")
 print()
 
-df = pd.DataFrame(pd.read_excel(file_path[0]))
+if a > 1:
+    df = pd.DataFrame(pd.read_excel(file_path[0]))
+    for element in file_path[1:9]:
+        dftemp = pd.DataFrame(pd.read_excel(element))
+        frames = [df, dftemp]
+        df = pd.concat(frames)
+        print(df)
+        print()
+
+else:
+    for element in file_path:
+        df = pd.DataFrame(pd.read_excel(element))
 print(df)
-print()
-
-for element in file_path[1:9]:
-    dftemp = pd.DataFrame(pd.read_excel(element))
-    frames = [df, dftemp]
-    df = pd.concat(frames)
-    print(df)
-    print()
-
-print(df)
-print()
 
 # Sektion 1
 b = 1
-sektion1 = "sektion_" + str(b) + ".xlsx"
-dfsek1 = df[(df.SEKTION == "Sektion 1: Historische und archäologische Wissenschaften")]
-dfsek1 = dfsek1.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
-dfsek1.to_excel(path + "/" + sektion1)
+found = df[df['SEKTION'].str.contains('Sektion 1: Historische und archäologische Wissenschaften')]
+v = len(found)
+
+if v > 0:
+    sektion1 = "sektion_" + str(b) + ".xlsx"
+    dfsek1 = df[(df.SEKTION == "Sektion 1: Historische und archäologische Wissenschaften")]
+    dfsek1 = dfsek1.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
+    dfsek1.to_excel(path + "/" + sektion1)
+else:
+    pass
 
 # Sektion 2
 b += 1
-sektion2 = "sektion_" + str(b) + ".xlsx"
-dfsek2 = df[(df.SEKTION == "Sektion 2: Kunstwissenschaften")]
-dfsek2 = dfsek2.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
-dfsek2.to_excel(path + "/" + sektion2)
+try:
+    sektion2 = "sektion_" + str(b) + ".xlsx"
+    dfsek2 = df[(df.SEKTION == "Sektion 2: Kunstwissenschaften")]
+    dfsek2 = dfsek2.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
+    dfsek2.to_excel(path + "/" + sektion2)
+except:
+    pass
 
 # Sektion 3
 b += 1
-sektion3 = "sektion_" + str(b) + ".xlsx"
-dfsek3 = df[(df.SEKTION == "Sektion 3: Sprach- und Literaturwissenschaften")]
-dfsek3 = dfsek3.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
-dfsek3.to_excel(path + "/" + sektion3)
+try:
+    sektion3 = "sektion_" + str(b) + ".xlsx"
+    dfsek3 = df[(df.SEKTION == "Sektion 3: Sprach- und Literaturwissenschaften")]
+    dfsek3 = dfsek3.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
+    dfsek3.to_excel(path + "/" + sektion3)
+except:
+    pass
 
 # Sektion 4
 b += 1
-sektion4 = "sektion_" + str(b) + ".xlsx"
-dfsek4 = df[(df.SEKTION == "Sektion 4: Kulturwissenschaften")]
-dfsek4 = dfsek4.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
-dfsek4.to_excel(path + "/" + sektion4)
+try:
+    sektion4 = "sektion_" + str(b) + ".xlsx"
+    dfsek4 = df[(df.SEKTION == "Sektion 4: Kulturwissenschaften")]
+    dfsek4 = dfsek4.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
+    dfsek4.to_excel(path + "/" + sektion4)
+except:
+    pass
 
 # Sektion 5
 b += 1
-sektion5 = "sektion_" + str(b) + ".xlsx"
-dfsek5 = df[(df.SEKTION == "Sektion 5: Wirtschafts- und Rechtswissenschaften")]
-dfsek5 = dfsek5.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
-dfsek5.to_excel(path + "/" + sektion5)
+try:
+    sektion5 = "sektion_" + str(b) + ".xlsx"
+    dfsek5 = df[(df.SEKTION == "Sektion 5: Wirtschafts- und Rechtswissenschaften")]
+    dfsek5 = dfsek5.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
+    dfsek5.to_excel(path + "/" + sektion5)
+except:
+    pass
 
 # Sektion 6
 b += 1
-sektion6 = "sektion_" + str(b) + ".xlsx"
-dfsek6 = df[(df.SEKTION == "Sektion 6: Gesellschaftswissenschaften")]
-dfsek6 = dfsek6.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
-dfsek6.to_excel(path + "/" + sektion6)
+try:
+    sektion6 = "sektion_" + str(b) + ".xlsx"
+    dfsek6 = df[(df.SEKTION == "Sektion 6: Gesellschaftswissenschaften")]
+    dfsek6 = dfsek6.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
+    dfsek6.to_excel(path + "/" + sektion6)
+except:
+    pass
 
 # Sektion 7
 b += 1
-sektion7 = "sektion_" + str(b) + ".xlsx"
-dfsek7 = df[(df.SEKTION == "Sektion 7: Wissenschaft – Technik – Gesellschaft")]
-dfsek7 = dfsek7.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
-dfsek7.to_excel(path + "/" + sektion7)
+try:
+    sektion7 = "sektion_" + str(b) + ".xlsx"
+    dfsek7 = df[(df.SEKTION == "Sektion 7: Wissenschaft – Technik – Gesellschaft")]
+    dfsek7 = dfsek7.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
+    dfsek7.to_excel(path + "/" + sektion7)
+except:
+    pass
 
 # Kommissionen / Kuratorien
-komm = "kommissionen_kuratorien.xlsx"
-dfsek7 = df[(df.SEKTION == "Kommission / Kuratorium")]
-dfsek7 = dfsek7.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
-dfsek7.to_excel(path + "/" + komm)
+try:
+    komm = "kommissionen_kuratorien.xlsx"
+    dfsek7 = df[(df.SEKTION == "Kommission / Kuratorium")]
+    dfsek7 = dfsek7.sort_values(by=["MITGLIEDINSTITUTION", "FORM_ID", "REFERENZ-NR."])
+    dfsek7.to_excel(path + "/" + komm)
+except:
+    pass
 
 print()
 print("Speichern abgeschlossen!")
